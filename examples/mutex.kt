c6604a5a74d0fe64e788d969ee8176532cdd7c78
Roman Elizarov <elizarov@gmail.com>
2016-12-20 13:40:02 +0300

    No need to have atomicity on `resumed` boolean

--- examples/mutex.kt
+++ examples/mutex.kt
@@ -1,7 +1,6 @@
 package mutex
 
 import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineIntrinsics
@@ -30,11 +29,15 @@ class Mutex {
                 val curState = state.get()
                 if (curState == -1) {
                     if (state.compareAndSet(-1, 0)) {
-                        // locked successfully this time, there were no _other_ waiter -- try mark us as resumed
-                        if (waiter.resumed.compareAndSet(false, true))
-                            return@sc Unit // don't suspend, but continue execution with lock
-                        // was already resumed by some other thread -> suspend
-                        break@loop
+                        // locked successfully this time, there were no _other_ waiter -- mark us as resumed,
+                        // but check we were already resummed in bettween waiters.add(...) and state.cas(...) by
+                        // somebody else
+                        if (waiter.resumed)
+                            break@loop // was already resumed by some other thread -> indicate suspend
+                        waiter.resumed = true // mark ourselves as already resumed in queue
+                        // for simplicity, don't attempt to unlink the  Waiter object from the queue
+                        return@sc Unit // don't suspend, but continue execution with lock
+
                     }
                 } else { // state >= 0 -- already locked --> increase waiters count and sleep peacefully until resumed
                     check(curState >= 0)
@@ -52,7 +55,7 @@ class Mutex {
             // see if can unlock
             val curState = state.get()
             if (curState == 0) {
-                // could not have any waiters in this state, because we are holding a mutex and only mutex-holder
+                // cannot have any waiters in this state, because we are holding a mutex and only mutex-holder
                 // can reduce the number of waiters
                 if (state.compareAndSet(0, -1))
                     return // successfully unlocked, no waiters were there to resume
@@ -60,7 +63,7 @@ class Mutex {
                 check(curState >= 1)
                 // now decrease waiters count and resume waiter
                 if (state.compareAndSet(curState, curState - 1)) {
-                    // must had a waiter!!
+                    // must have a waiter!!
                     retrieveWaiter()!!.c.resume(Unit)
                     return
                 }
@@ -72,13 +75,13 @@ class Mutex {
         while (true) {
             val waiter = waiters.poll() ?: return null
             // see if this is an _actual_ waiter (not resumed yet by some previous mutex holder)
-            if (waiter.resumed.compareAndSet(false, true))
+            if (!waiter.resumed)
                 return waiter
             // otherwise it is an artefact, just look for the next one
         }
     }
 
     private class Waiter(val c: Continuation<Unit>) {
-        val resumed = AtomicBoolean()
+        var resumed = false
     }
 }
