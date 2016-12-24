755e3c2e8a391dda1bb18ebf9301d9e854d8e1cb
Roman Elizarov <elizarov@gmail.com>
2016-12-24 20:00:11 +0300

    Better asyncGenerate. Explicitly tracks state, fails fast on recursion.

--- examples/asyncGenerate.kt
+++ examples/asyncGenerate.kt
@@ -24,74 +24,79 @@ fun <T> asyncGenerate(block: suspend AsyncGenerator<T>.() -> Unit): AsyncSequenc
 }
 
 class AsyncGeneratorIterator<T>: AsyncIterator<T>, AsyncGenerator<T>, Continuation<Unit> {
-    var computedNext = false
+    enum class State { INITIAL, COMPUTING_HAS_NEXT, COMPUTING_NEXT, COMPUTED, DONE }
+    var state: State = State.INITIAL
     var nextValue: T? = null
-    var nextStep: Continuation<Unit>? = null
+    var nextStep: Continuation<Unit>? = null // null when sequence complete
 
-    // if (computesNext) computeContinuation is Continuation<T>
-    // if (!computesNext) computeContinuation is Continuation<Boolean>
-    var computesNext = false
+    // if (state == COMPUTING_NEXT) computeContinuation is Continuation<T>
+    // if (state == COMPUTING_HAS_NEXT) computeContinuation is Continuation<Boolean>
     var computeContinuation: Continuation<*>? = null
 
     suspend fun computeHasNext(): Boolean = suspendCoroutine { c ->
-        computesNext = false
+        state = State.COMPUTING_HAS_NEXT
         computeContinuation = c
         nextStep!!.resume(Unit)
     }
 
     suspend fun computeNext(): T = suspendCoroutine { c ->
-        computesNext = true
+        state = State.COMPUTING_NEXT
         computeContinuation = c
         nextStep!!.resume(Unit)
     }
 
-    @Suppress("UNCHECKED_CAST")
-    fun resumeIterator(exception: Throwable?) {
-        if (exception != null) {
-            done()
-            computeContinuation!!.resumeWithException(exception)
-            return
-        }
-        if (computesNext) {
-            computedNext = false
-            (computeContinuation as Continuation<T>).resume(nextValue as T)
-        } else {
-            (computeContinuation as Continuation<Boolean>).resume(nextStep != null)
-        }
-    }
-
     override suspend fun hasNext(): Boolean {
-        if (!computedNext) return computeHasNext()
-        return nextStep != null
+        when (state) {
+            State.INITIAL -> return computeHasNext()
+            State.COMPUTED -> return true
+            State.DONE -> return false
+            else -> throw IllegalStateException("Recursive dependency detected -- already computing next")
+        }
     }
 
     override suspend fun next(): T {
-        if (!computedNext) return computeNext()
-        computedNext = false
-        return nextValue as T
+        when (state) {
+            State.INITIAL -> return computeNext()
+            State.COMPUTED -> {
+                state = State.INITIAL
+                return nextValue as T
+            }
+            State.DONE -> throw NoSuchElementException()
+            else -> throw IllegalStateException("Recursive dependency detected -- already computing next")
+        }
     }
 
-    private fun done() {
-        computedNext = true
-        nextStep = null
+    @Suppress("UNCHECKED_CAST")
+    fun resumeIterator(hasNext: Boolean) {
+        when (state) {
+            State.COMPUTING_HAS_NEXT -> {
+                state = State.COMPUTED
+                (computeContinuation as Continuation<Boolean>).resume(hasNext)
+            }
+            State.COMPUTING_NEXT -> {
+                state = State.INITIAL
+                (computeContinuation as Continuation<T>).resume(nextValue as T)
+            }
+            else -> throw IllegalStateException("Was not supposed to be computing next value. Spurious yield?")
+        }
     }
 
     // Completion continuation implementation
     override fun resume(value: Unit) {
-        done()
-        resumeIterator(null)
+        nextStep = null
+        resumeIterator(false)
     }
 
     override fun resumeWithException(exception: Throwable) {
-        done()
-        resumeIterator(exception)
+        nextStep = null
+        state = State.DONE
+        computeContinuation!!.resumeWithException(exception)
     }
 
     // Generator implementation
     override suspend fun yield(value: T): Unit = suspendCoroutine { c ->
-        computedNext = true
         nextValue = value
         nextStep = c
-        resumeIterator(null)
+        resumeIterator(true)
     }
 }
